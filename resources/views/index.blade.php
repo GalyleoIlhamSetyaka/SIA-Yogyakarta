78a3e10644017f90d78e7f06f2076574d192cdb9
GalyleoIlhamSetyaka <ilhamsetyaka14@gmail.com>
2025-03-19 19:05:00 +0700

    tailwind styleing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,13 +31,43 @@
         <div class="h-screen flex items-center">
             <div class="container mx-auto flex justify-between items-center">
                 <div class="w-1/2">
-                    <div class="absolute top-0 left-0 w-full h-screen bg-[url('{{ asset('img/index/YIA.png') }}')] bg-cover bg-center" style="filter: blur(5px); filter: contrast(-200); filter: brightness(-50); "></div>
+                    <div class="absolute top-0 left-0 w-full h-full bg-[url('{{ asset('img/index/YIA.png') }}')] bg-cover bg-center" style="filter: blur(5px);"></div>
                     <h1 class="text-6xl font-bold text-blue-500 relative z-10">Welcome to Yogyakarta International Airport</h1>
-                    <p class="text-xl mt-4 text-white relative z-10">The airport is located in the heart of Yogyakarta, Indonesia. It is the third-busiest airport in the country based on the aircraft movements and passenger movements.</p>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 relative z-10">Learn More</button> 
+                    <p class="text-xl text-white relative z-10">The airport is located in the heart of Yogyakarta, Indonesia. It is the third-busiest airport in the country based on the aircraft movements and passenger movements.</p>
                 </div>
             </div>
         </div>
     </section>
+    
+    <section class="bg-white py-12">
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-12 gap-8">
+        <div class="col-span-12 md:col-span-6 relative">
+                <img src="{{ asset('img/index/Plane2.png') }}" alt="Pesawat" class="w-full">
+            </div>
+            <div class="col-span-12 md:col-span-6">
+                <p class="text-blue-700 text-lg">
+                    Bandar Udara Internasional Yogyakarta (IATA: YIA, ICAO: WAHI) adalah sebuah Bandar Udara Internasional yang terletak 45 kilometer dari Kota Yogyakarta tepatnya di Kapanewon Temon, Kulon Progo. YIA menggantikan Bandar Udara Internasional Adisucipto (JOG) yang dinilai sudah tidak mampu lagi menampung kapasitas penumpang dan pesawat. Bandar YIA melayani penerbangan domestik ke beberapa kota-kota di Indonesia serta penerbangan internasional ke Kuala Lumpur dan Singapura. Per 29 April 2024, bandar udara ini merupakan satu-satunya bandar udara internasional untuk Daerah Istimewa Yogyakarta dan seluruh Provinsi Jawa Tengah.
+                </p>
+            </div>
+
+        </div>
+    </div>
+    </section>
+
+    <footer class="bg-blue-500 py-4">
+        <div class="container mx-auto px-4 flex justify-between items-center">
+            <div>
+                <img src="{{ asset('img/index/TAG.png') }}" alt="Logo Footer" class="h-10">
+            </div>
+            <div class="text-sm text-white text-align-center">
+                <p>Andalan, Temon</p>
+                <p>Kulon Progo, Yogyakarta</p>
+                <p>Indonesia - 56554</p>
+                <p>Telp: (0274) 6488072 | Fax: (0274) 4606001</p>
+                <p>cs172@ap1.co.id</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
